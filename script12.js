0a4e7c879a8b15b4eb724c58a0011f76af4e2489
yashpokle <yashpokle277@gmail.com>
2024-10-18 00:13:00 +0530

    Add files via upload

---
+++ script12.js
@@ -0,0 +1,4 @@
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Hold On...! Searching best Results for your Location  ');
+});
